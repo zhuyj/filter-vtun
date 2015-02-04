f96c9b9c3e8ed14cd399e1fc703a432376d7a22a
zhuyj <zyjzyj2000@gmail.com>
2015-02-04 16:26:51 +0800

    operate:create operate.c and Makefile
    operate.c creates vtun0 virtual network interface after running
    "insmod vtun.ko". The command:
    ./operate add  <--insert vtun0
    ./operate del  <--del vtun0

---
+++ interface/operate.c
@@ -0,0 +1,57 @@
+#include <net/if.h>
+#include <linux/if_tun.h>
+#include <asm-generic/fcntl.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int tun_alloc(char *dev, int addordel)
+{
+	struct ifreq ifr;
+	int fd, err;
+
+	if((fd = open("/dev/net/vtun", O_RDWR)) < 0 )
+		return -1;
+
+	memset(&ifr, 0, sizeof(ifr));
+
+      /* Flags: IFF_TUN   - TUN device (no Ethernet headers) 
+       *        IFF_TAP   - TAP device  
+       *
+       *        IFF_NO_PI - Do not provide packet information  
+       */
+	ifr.ifr_flags |= IFF_TUN; 
+	if( *dev )
+		strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+
+	if((err = ioctl(fd, TUNSETIFF, &ifr)) < 0){
+		close(fd);
+		return err;
+	}
+	strcpy(dev, ifr.ifr_name);
+	if ((err = ioctl(fd, TUNSETPERSIST, addordel)) < 0){
+		close(fd);
+		return err;	
+	}
+	close(fd);
+	return err;
+}       
+int main(int argc, char *argv[])
+{
+	char ifname[32] = {0};
+	
+	if (argc < 2){
+		printf("error!argc < 2");
+		exit(0);
+	}
+	printf("%s\n", argv[1]);
+	strcpy(ifname, "vtun0");
+	if (strcmp(argv[1], "add") == 0){
+		tun_alloc(ifname, 1);
+	}
+	if (strcmp(argv[1], "del") == 0){
+		tun_alloc(ifname, 0);
+	}
+	return 0;
+}       

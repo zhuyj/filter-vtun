2b9b97b8eef25bf192487b52ce6ecb2f9ef60039
zhuyj <zyjzyj2000@gmail.com>
2015-02-10 18:16:26 +0800

    operate: remove Wall warning
    In Makefile, Wall option is inserted. To remove warnings, some
    header files are inserted.

--- interface/operate.c
+++ interface/operate.c
@@ -1,10 +1,15 @@
 #include <net/if.h>
 #include <linux/if_tun.h>
-#include <asm-generic/fcntl.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int tun_alloc(char *dev, int addordel)
 {
@@ -36,7 +41,8 @@ int tun_alloc(char *dev, int addordel)
 	}
 	close(fd);
 	return err;
-}       
+}
+
 int main(int argc, char *argv[])
 {
 	char ifname[32] = {0};
